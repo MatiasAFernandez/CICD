fcac5e6895080447c0912df98e2562bae33232c0
MatiasAFernandez <matiasafernandez23@gmail.com>
2025-05-18 12:26:27 -0300

    prueba1: falla del test

--- test.js
+++ test.js
@@ -2,5 +2,5 @@ const assert = require('assert');
 const suma = require('./index');
 
 // Prueba simple
-assert.strictEqual(suma(2, 3), 5);
+assert.strictEqual(suma(2, 3), 6);
 console.log("✅ Test pasado");
