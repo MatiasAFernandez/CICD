c5a67c563986e07ed0aa976de984ab3d455bc75c
MatiasAFernandez <matiasafernandez23@gmail.com>
2025-05-18 12:47:52 -0300

    CI

--- test.js
+++ test.js
@@ -2,5 +2,5 @@ const assert = require('assert');
 const suma = require('./index');
 
 // Prueba simple
-assert.strictEqual(suma(2, 3), 6);
+assert.strictEqual(suma(2, 3), 5);
 console.log("✅ Test pasado");
