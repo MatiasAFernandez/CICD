9d00be35d35728047b6dad96f096f73345d3b4bb
MatiasAFernandez <matiasafernandez23@gmail.com>
2025-06-12 16:17:13 -0300

    bun falla

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 export function lanzarDado() {
-  return Math.floor(Math.random() * 6) + 1; // Número entre 1 y 6
+  return Math.floor(Math.random() * 6) + 7; // Número entre 1 y 6
 }
 
 // Manejar el evento del botón
